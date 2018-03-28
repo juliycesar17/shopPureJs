04f3209d54b116955d933afc87d7d93966a71951
Vasiliy <23014160+juliycesar17@users.noreply.github.com>
2018-03-29 00:52:43 +0300

    Update controllerclass.js

--- controller/controllerclass.js
+++ controller/controllerclass.js
@@ -1,16 +1,18 @@
 function ListControllerParent() {
-	
+
     this._model = arguments[0];
     this._view = arguments[1];
-	this.ard = arguments[2] || '{prop:prop}';
+	  this.ard = arguments[2] || '{prop:prop}';
 }
 
 ListControllerParent.prototype.newState = function () {
 	let _this = this;
-	let argos = arguments;
-	let el = arguments[0].url;
+	let args = arguments;
+	let phpScript = arguments[0].phpScript;
+	console.log(phpScript);
 	let title = arguments[0].title;
-	return this._model.ajax(argos[0]).then(function( msg ) {
-		return _this._model.addItem( msg, el, title )
+	return this._model.ajax(args[0]).then(function( response ) {
+
+		return _this._model.loadData( response, phpScript, title )
 	});
 }
