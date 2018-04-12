52497536706d31d708f2deaf631f2aa5caedcff9
Vasiliy <23014160+juliycesar17@users.noreply.github.com>
2018-04-12 12:08:57 +0300

    Update ind.js

--- ind.js
+++ ind.js
@@ -1,19 +1,19 @@
 (function(){ history.pushState('http://www.joyforchild.com/ice/index.php', document.title, 'http://www.joyforchild.com/ice/index.php');})();
 	 
-	 index();
-	 
-	 function index() {
-		 
-	   if($("#fff").length > 0) {
-	   $('#fff').remove();
-	 }
-	 
-	   if($("#ggg").length > 0) {
-	   $('#ggg').remove();
-	 }
-	 
-	 $('[class~=aaa]').remove();
-	 
+index();
+
+function index() {
+
+	if($("#fff").length > 0) {
+		$('#fff').remove();
+	}
+
+	if($("#ggg").length > 0) {
+		$('#ggg').remove();
+	}
+
+	$('[class~=aaa]').remove();
+
 	var source = $("#index").html();
 	var template = Handlebars.compile(source);
 	var htmll = template();
